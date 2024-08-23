b2a6f93011e92d5965ff15b0c49645a7ffe5f713
Ousama Abou Khalil <131563820+OusamaAbouKhalil@users.noreply.github.com>
2024-08-24 00:02:09 +0300

    Update Menu.jsx

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -79,7 +79,7 @@ export default function Menu() {
                 </td>
                 <td className="py-4 px-6">
                   <Link
-                    to={`/restaurants/${id}/${item.item_id}`}
+                    to={`/restaurants/${id}/${item.item_id}`} // Solved By Ousama
                     className="font-medium text-gray-900 dark:text-white hover:underline"
                   >
                     {item.item_name}
