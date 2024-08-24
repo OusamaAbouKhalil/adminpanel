ebd70ab051e231f35d6561eeda582944b0d53276
Ousama Abou Khalil <131563820+OusamaAbouKhalil@users.noreply.github.com>
2024-08-24 15:43:07 +0300

    Update OrdersTable.jsx

--- src/components/Orders/OrdersTable.jsx
+++ src/components/Orders/OrdersTable.jsx
@@ -17,8 +17,8 @@ const OrdersTable = ({ orders, onStatusChange }) => {
           <div key={status} className="relative inline-block mr-4">
             <button
               key={status}
-              className={rounded-t-lg p-2 mr-2 lg:text-lg text-sm font-bold text-gray-700 ${activeTab === status ? "bg-gray-200" : "bg-white"
-                }}
+              className={`rounded-t-lg p-2 mr-2 lg:text-lg text-sm font-bold text-gray-700 ${activeTab === status ? "bg-gray-200" : "bg-white"
+                }`}
               onClick={() => setActiveTab(status)}
             >
               {status.toUpperCase()}
@@ -38,8 +38,8 @@ const OrdersTable = ({ orders, onStatusChange }) => {
       {statuses.map((status) => (
         <div
           key={status}
-          className={${activeTab === status ? "block" : "hidden"
-            } shadow-lg rounded-lg p-4 bg-white}
+          className={`${activeTab === status ? "block" : "hidden"
+            } shadow-lg rounded-lg p-4 bg-white`}
         >
           <h2 className="text-lg font-bold text-gray-700 mb-4">
             {status.toUpperCase()}
