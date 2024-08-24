c88665293ca23bda3eb06b8235fa0cb63b21812c
Ousama Abou Khalil <131563820+OusamaAbouKhalil@users.noreply.github.com>
2024-08-25 00:27:05 +0300

    Update ProtectedRoutes.jsx

--- src/contexts/ProtectedRoutes.jsx
+++ src/contexts/ProtectedRoutes.jsx
@@ -35,6 +35,7 @@ export const ProtectedRoute = ({ children }) => {
             'add',
             'prices',
             'notification',
+            'editnotification',
         ]
         if (!adminRoutes.includes(location.pathname.split('/')[1])) { return <Alert className="mx-4" variant="filled" severity="error">Access Denied</Alert> };
     }
