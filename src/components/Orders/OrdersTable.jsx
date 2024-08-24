9b02445ab977485e321eafa124a4ced78bd4635d
Ousama Abou Khalil <131563820+OusamaAbouKhalil@users.noreply.github.com>
2024-08-24 15:39:57 +0300

    Update OrdersTable.jsx

--- src/components/Orders/OrdersTable.jsx
+++ src/components/Orders/OrdersTable.jsx
@@ -1,45 +1,42 @@
 import React, { useState } from "react";
+import { format } from "date-fns"; // For formatting timestamps
 
 const OrdersTable = ({ orders, onStatusChange }) => {
-  const statuses = ["accepted", "preparing", "on the way", "completed","cancelled","rejected"];
+  const statuses = ["accepted", "preparing", "on the way", "completed", "rejected", "cancelled"];
   const [activeTab, setActiveTab] = useState(statuses[0]); // Default to the first status
-  const [searchTerm, setSearchTerm] = useState(""); // For filtering by Order ID
+  const [searchTerm, setSearchTerm] = useState("");
 
   const getStatusCount = (status) => {
     return orders.filter((order) => order.status === status).length;
   };
 
-  const filteredOrders = orders.filter((order) => 
-    order.status === activeTab && order.order_id.includes(searchTerm)
-  );
-
-  const formatDateTime = (timestamp) => {
-    const date = new Date(timestamp.seconds * 1000); // Convert Firestore timestamp to JS Date
-    const formattedDate = date.toLocaleDateString("en-US");
-    const formattedTime = date.toLocaleTimeString("en-US", {
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: true,
-    });
-    return `${formattedDate}, ${formattedTime}`;
+  const formatTimestamp = (timestamp) => {
+    return format(new Date(timestamp.seconds * 1000), "dd/MM/yyyy HH:mm");
   };
-  
-    // Sorting orders by Firestore timestamp
-  const sortedOrders = orders
-    .filter((order) => order.status === activeTab && order.order_id.includes(searchTerm))
-    .sort((a, b) => {
-      return b.time.seconds - a.time.seconds;
-    });
+
+  const filteredOrders = orders
+    .filter((order) => order.status === activeTab)
+    .filter((order) => order.order_id.toLowerCase().includes(searchTerm.toLowerCase()))
+    .sort((a, b) => b.time.seconds - a.time.seconds);
 
   return (
-    <div className="my-10 p-4">
-      <div className="flex justify-center mb-6">
-        {/* Center the status buttons */}
+    <div className="my-10">
+      <div className="flex justify-center mb-4">
+        {/* Search bar */}
+        <input
+          type="text"
+          placeholder="Search by Order ID"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="border rounded p-2 mr-4"
+        />
+
+        {/* Status buttons */}
         {statuses.map((status) => (
           <div key={status} className="relative inline-block mr-4">
             <button
-              key={status}
-              className={`rounded-t-lg p-3 mr-2 lg:text-lg text-sm font-bold text-gray-700 ${activeTab === status ? "bg-gray-200" : "bg-white"} transition-colors duration-300`}
+              className={`rounded-t-lg p-2 mr-2 lg:text-lg text-sm font-bold text-gray-700 ${activeTab === status ? "bg-gray-200" : "bg-white"
+                }`}
               onClick={() => setActiveTab(status)}
             >
               {status.toUpperCase()}
@@ -56,92 +53,66 @@ const OrdersTable = ({ orders, onStatusChange }) => {
         ))}
       </div>
 
-      <div className="mb-6 flex justify-center">
-        {/* Search bar for Order ID */}
-        <input
-          type="text"
-          placeholder="Search by Order ID..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="p-2 border border-gray-300 rounded-lg w-full max-w-md"
-        />
-      </div>
-
-      {statuses.map((status) => (
-        <div
-          key={status}
-          className={`${activeTab === status ? "block" : "hidden"} shadow-lg rounded-lg p-6 bg-white`}
-        >
-          <h2 className="text-lg font-bold text-gray-800 mb-6">
-            {status.toUpperCase()}
-          </h2>
-          <div className="overflow-x-auto">
-            {/* Add horizontal scroll for smaller screens */}
-            <table className="min-w-full table-fixed border-separate border-spacing-0">
-              <thead className="bg-gray-200 text-gray-700">
-                <tr>
-                  {status !== "accepted" || status !== "rejected" || status !== "cancelled" && (
-                    <th className="px-4 py-2 border-b border-gray-300 w-1/6 text-left">Actions</th>
-                  )}
-                  <th className="px-4 py-2 border-b border-gray-300 w-1/6 text-left">Order ID</th>
-                  <th className="px-4 py-2 border-b border-gray-300 w-1/6 text-left">Recipient</th>
-                  <th className="px-4 py-2 border-b border-gray-300 w-1/6 text-left">Total</th>
-                  <th className="px-4 py-2 border-b border-gray-300 w-1/6 text-left">Date</th> {/* New column */}
-                  <th className="px-4 py-2 border-b border-gray-300 w-1/6 text-left">Status</th>
-                  {status !== "completed" && (
-                    <th className="px-4 py-2 border-b border-gray-300 w-1/6 text-left">Actions</th>
+      <div className="shadow-lg rounded-lg p-4 bg-white">
+        <h2 className="text-lg font-bold text-gray-700 mb-4">
+          {activeTab.toUpperCase()}
+        </h2>
+        <div className="overflow-x-auto">
+          <table className="min-w-full table-fixed">
+            <thead className="bg-gray-100">
+              <tr>
+                <th className="px-4 py-2 w-1/4">Order ID</th>
+                <th className="px-4 py-2 w-1/4">Recipient</th>
+                <th className="px-4 py-2 w-1/4">Total</th>
+                <th className="px-4 py-2 w-1/4">Status</th>
+                <th className="px-4 py-2 w-1/4">Date</th>
+                {activeTab !== "rejected" && activeTab !== "cancelled" && activeTab !== "completed" && (
+                  <th className="px-4 py-2 w-1/4">Actions</th>
+                )}
+              </tr>
+            </thead>
+            <tbody>
+              {filteredOrders.map((order) => (
+                <tr
+                  key={order.order_id}
+                  className="border-b last:border-b-0"
+                >
+                  <td className="border px-4 py-2 sm:text-base text-xs">
+                    {order.order_id}
+                  </td>
+                  <td className="border px-4 py-2 sm:text-base text-xs">
+                    {order.recipient_name}
+                  </td>
+                  <td className="border px-4 py-2 sm:text-base text-xs">
+                    ${order.total + order.delivery_fee}
+                  </td>
+                  <td className="border px-4 py-2 sm:text-base text-xs">
+                    {order.status}
+                  </td>
+                  <td className="border px-4 py-2 sm:text-base text-xs">
+                    {formatTimestamp(order.time)}
+                  </td>
+                  {activeTab !== "rejected" && activeTab !== "cancelled" && activeTab !== "completed" && (
+                    <td className="border px-4 py-2">
+                      <button
+                        onClick={() =>
+                          onStatusChange(
+                            order,
+                            statuses[statuses.indexOf(activeTab) + 1]
+                          )
+                        }
+                        className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      >
+                        Move to {statuses[statuses.indexOf(activeTab) + 1]}
+                      </button>
+                    </td>
                   )}
                 </tr>
-              </thead>
-              <tbody>
-                {filteredOrders.map((order) => (
-                  <tr
-                    key={order.order_id}
-                    className="border-b last:border-b-0 hover:bg-gray-100 transition-colors duration-300"
-                  >
-                    {status !== "accepted" && (
-                      <td className="border px-4 py-2 text-sm">
-                        <button
-                          onClick={() =>
-                            onStatusChange(
-                              order,
-                              statuses[statuses.indexOf(status) - 1]
-                            )
-                          }
-                          className="bg-blue-500 hover:bg-blue-700 text-white text-xs font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        >
-                          Move to {statuses[statuses.indexOf(status) - 1]}
-                        </button>
-                      </td>
-                    )}
-                    <td className="border px-4 py-2 text-sm">{order.order_id}</td>
-                    <td className="border px-4 py-2 text-sm">{order.recipient_name}</td>
-                    <td className="border px-4 py-2 text-sm">${order.total + order.delivery_fee}</td>
-                     <td className="border px-4 py-2 text-sm">{formatDateTime(order.time)}</td> {/* New column */}
-                     <td className="border px-4 py-2 text-sm">{order.status}</td>
-
-                    {status !== "completed" || status !== "rejected" || status !== "cancelled" && (
-                      <td className="border px-4 py-2 text-sm">
-                        <button
-                          onClick={() =>
-                            onStatusChange(
-                              order,
-                              statuses[statuses.indexOf(status) + 1]
-                            )
-                          }
-                          className="bg-blue-500 hover:bg-blue-700 text-white text-xs font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        >
-                          Move to {statuses[statuses.indexOf(status) + 1]}
-                        </button>
-                      </td>
-                    )}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+              ))}
+            </tbody>
+          </table>
         </div>
-      ))}
+      </div>
     </div>
   );
 };
